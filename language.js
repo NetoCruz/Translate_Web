8edfef871198e4f1060cd668523976d6e6ca432b
NetoCruz <galaxy.net0710@gmail.com>
2018-03-02 14:59:43 -0600

    idioma_2

--- language.js
+++ language.js
@@ -1,9 +1,16 @@
 function myFunction() {
     var x = document.getElementById("myText").value.toString();
       
-    dividir(x)
-
     
+
+    if (document.getElementById("r1").checked) {
+    
+    dividir(x)
+ }
+ if (document.getElementById("r2").checked) {
+    
+    dividir2(x)
+ }
  
 
 
@@ -18,6 +25,61 @@ function clearmi() {
 
 }
 
+////////////////////////////////////////////////////////////
+var odd = []
+
+function dividir2(str){
+ var array= str
+ const res=array.split(" ")
+ res.forEach(change)
+document.getElementById("demo").value =odd.join(" ");
+ console.log(`${odd}`)
+ 
+}
+
+function change(str){
+ const word= str
+  var odd_numbers = " "
+ odd_numbers=word
+ 
+  if (odd_numbers.includes("o")) {
+    
+    //const v=word.replace("o", "ofo")
+    odd_numbers=odd_numbers.replace("o", "ofo");
+    
+    //console.log(`${v}`)
+   }
+   if (odd_numbers.includes("a")) {
+    
+  //const v=word.replace("o", "ofo")
+    odd_numbers=odd_numbers.replace("a", "afa");
+    
+    //console.log(`${v}`)
+   }
+   if (odd_numbers.includes("i")) {
+    
+  //const v=word.replace("o", "ofo")
+    odd_numbers=odd_numbers.replace("i", "ifi");
+    
+    //console.log(`${v}`)
+   }
+   if (odd_numbers.includes("e")) {
+    
+  //const v=word.replace("o", "ofo")
+    odd_numbers=odd_numbers.replace("e", "efe");
+    
+    //console.log(`${v}`)
+   }
+
+odd.push(odd_numbers)
+
+//console.log(`${odd_numbers}`)
+}
+//////////////////////////////////////////////////////////////
+
+
+
+
 
 var odd_numbers = []
 
@@ -31,6 +93,7 @@ console.log(`${odd_numbers}`)
 
 }
 
+
 function translate(str){
 	const word= str
 	
@@ -38,13 +101,16 @@ function translate(str){
 
 }
 
+
+
 $('input[value="1"]').on('change', function() {
     $('label[id="feedback"]').text("idioma");
     $(botones).css({ "background-color": "#533A71" });
+
 });
 
 $('input[value="2"]').on('change', function() {
-    $('label[id="feedback"]').text("idioma2");
+    $('label[id="feedback"]').text("Idioma de la F");
     $(botones).css({ "background-color": "#6184D8" });
    
 });
